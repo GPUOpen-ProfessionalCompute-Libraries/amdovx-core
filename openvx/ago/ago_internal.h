102af14cda3d1f1cd25b2b990f6ff9a56f895881
Radha Giduthuri <rgiduthuri@gmail.com>
2017-09-29 21:29:40 -0700

    updated version to 0.9.7

--- openvx/ago/ago_internal.h
+++ openvx/ago/ago_internal.h
@@ -34,7 +34,7 @@ THE SOFTWARE.
 //
 
 // version
-#define AGO_VERSION "0.9.6"
+#define AGO_VERSION "0.9.7"
 
 // debug configuration
 #define ENABLE_DEBUG_MESSAGES                 0 // 0:disable 1:enable
